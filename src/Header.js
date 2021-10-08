5cffaa4d0e3147f7e78ff72d08447e6ab7cea391
Roberto <robertofelixquintana@gmail.com>
2021-10-07 21:40:28 -0700

    Updates

--- src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-    return (
-        <div className="Header">
-            <img src="https://danielasanchez.github.io/react_practice1_20212/static/media/logo.da78bd05.jpg" alt="" className="img"></img>
-        </div>
-    )
-}
